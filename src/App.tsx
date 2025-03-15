9e5df0917214abdfa930ef10fa6b97f7df8b9a13
Superkid <kevinchristensen76923@gmail.com>
2025-03-15 23:43:02 +0700

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { NavLink, Outlet } from "react-router";
+import Layout from "./layout";
 
 function App() {
 	return (
 		<>
-			<div className="space-x-4">
-				<NavLink to={"/root/about"}>About me</NavLink>
-				<NavLink to={"/root/intro"}>Introduce</NavLink>
-				<NavLink to={"/root/teoem/:id"}>Teo em</NavLink>
-				<NavLink to={"/root"}>Root</NavLink>
-			</div>
-			<Outlet></Outlet>
+			{/* <div className="flex flex-wrap">
+				<div className="space-x-4 flex flex-col border-2 border-solid flex-1/6">
+					<NavLink to={"/root"}>Changelog</NavLink>
+					<NavLink to={"/root/about"}>Getting Started</NavLink>
+					<NavLink to={"/root/intro"}>Introduce</NavLink>
+					<NavLink to={"/root/teoem/:id"}>Teo em</NavLink>
+				</div>
+				<div className="border-2 border-solid flex-5/6">
+					<Outlet></Outlet>
+				</div>
+			</div> */}
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import App from "./App";
 import About from "./about";
 import Teoem from "./teoem";
 import Intro from "./intro";
+import Layout from "./layout";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/root" element={<App />}>
+				<Route path="/root"></Route>
+				<Route path="/root" element={<Layout children={undefined} />}>
 					<Route path="intro" element={<Intro />} />
 					<Route path="about" element={<About />} />
 					<Route path="teoem/:id" element={<Teoem />} />
